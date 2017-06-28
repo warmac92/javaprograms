ac49f0cb61b680e180e370d66d376aba2f8d2009
Abhishek Shrivatava <warmac92@gmail.com>
2017-06-28 13:24:49 -0400

    initial commit and added 3 programs

---
+++ Dowhile.java
@@ -0,0 +1,11 @@
+public class Dowhile{
+
+   public static void main(String args[]) {
+      int a = 1;
+
+      do {
+         System.out.println("value of a : " + a );
+         a++;
+      }while( a < 10 );
+   }
+}
